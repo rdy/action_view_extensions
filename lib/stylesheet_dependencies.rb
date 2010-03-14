481c497496cd482d4d5873ff7942537ce969779a
Ryan Dy <ryan.dy@gmail.com>
2010-03-14 10:08:28 -0700

    updating path to root from plugin

--- lib/stylesheet_dependencies.rb
+++ lib/stylesheet_dependencies.rb
@@ -1,7 +1,7 @@
 module StylesheetDependencies
   def stylesheet_dependencies
-    stylesheets = Dir[File.join(File.dirname(__FILE__), '..', 'public', 'stylesheets', '**', '*.css')].collect {|a| a.gsub(/^.*\/public(.*)$/, '\1') }
-    YAML.load_file(File.join(File.dirname(__FILE__), '..', 'config', 'stylesheets.yml')).collect do |a|
+    stylesheets = Dir[File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'public', 'stylesheets', '**', '*.css')].collect {|a| a.gsub(/^.*\/public(.*)$/, '\1') }
+    YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'config', 'stylesheets.yml')).collect do |a|
       stylesheets.detect { |b| b =~ /#{a}\.css$/ }
     end.concat(stylesheets).compact.uniq
   end
