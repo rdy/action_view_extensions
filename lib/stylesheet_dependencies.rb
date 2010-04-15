9d721176fa582d7185e7622292649768f34cf5e8
Ryan Dy <ryan.dy@gmail.com>
2010-04-15 14:15:46 -0700

    allowing order and reject if configured as a hash

--- lib/stylesheet_dependencies.rb
+++ lib/stylesheet_dependencies.rb
@@ -1,8 +1,17 @@
 module StylesheetDependencies
-  def stylesheet_dependencies
-    stylesheets = Dir[File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'public', 'stylesheets', '**', '*.css')].collect {|a| a.gsub(/^.*\/public(.*)$/, '\1') }
-    YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'config', 'stylesheets.yml')).collect do |a|
-      stylesheets.detect { |b| b =~ /#{a}\.css$/ }
-    end.concat(stylesheets).compact.uniq
-  end
+ def stylesheet_dependencies
+   stylesheets = Dir[File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'public', 'stylesheets', '**', '*.css')].collect {|a| a.gsub(/^.*\/public(.*)$/, '\1') }
+   config = YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'config', 'stylesheets.yml'))
+   order, rejects = if config.is_a?(Hash)
+     [config['order'], config['reject']]
+   else
+     [config, []]
+   end
+
+   order.collect do |a|
+     stylesheets.detect { |b| File.basename(b, '.css') == a }
+   end.concat(stylesheets).reject do |a|
+     rejects.detect { |b| File.basename(a, '.css') == b }
+   end.compact.uniq
+ end
 end
